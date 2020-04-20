0a5970562e2c0aa0a1580753546fea4e2ef5df82
Fariha <farihabaloch@gmail.com>
2020-04-19 20:31:10 -0600

    Updated after API error

--- YouTube_Data_Wrangling.py
+++ YouTube_Data_Wrangling.py
@@ -45,88 +45,162 @@ mx_df.info()
 # In[6]:
 
 
-import requests
-temp_category_usa={}
-temp_category_mx={}
-
-url="https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=US&key=AIzaSyDj7XbNnUjmUHGKxrPEy_KBmuh6J5gDoes"
-res=requests.get(url)
-cat_json_usa=res.json()
+#The following code did run and then I saved the final dictionaries in category_mx and category_usa. The API key cannot be
+#shared as I did before and seems like someone took it and running it crazily everyday making my daily quota exceed 10K hits. 
 
 
-url1="https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=MX&key=AIzaSyDj7XbNnUjmUHGKxrPEy_KBmuh6J5gDoes"
-res1=requests.get(url1)
-cat_json_mx=res1.json()
+#import requests
+#temp_category_usa={}
+#temp_category_mx={}
+
+#API_key=
+
+#url="https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=US&key=API_key"
+#res=requests.get(url)
+#cat_json_usa=res.json()
+
+#url1="https://www.googleapis.com/youtube/v3/videoCategories?part=snippet&regionCode=MX&key=API_key"
+#res1=requests.get(url1)
+#cat_json_mx=res1.json()
 
 
-for i in range(len(cat_json_usa['items'])):
-    temp_category_usa[cat_json_usa['items'][i]['id']]=cat_json_usa['items'][i]['snippet']['title']
-#category_usa
+
+#for i in range(len(cat_json_usa['items'])):
+#    temp_category_usa[cat_json_usa['items'][i]['id']]=cat_json_usa['items'][i]['snippet']['title']
+#temp_category_usa
 
 
-for i in range(len(cat_json_mx['items'])):
-    temp_category_mx[cat_json_mx['items'][i]['id']]=cat_json_mx['items'][i]['snippet']['title']
-#category_mx
+#for i in range(len(cat_json_mx['items'])):
+#    temp_category_mx[cat_json_mx['items'][i]['id']]=cat_json_mx['items'][i]['snippet']['title']
+#temp_category_mx
 
 #The temp_category_usa and temp_category_mx produce a dictionary with keys as strings and I need to redo that to have an integer value i.e.
 #without ' ' around the number. If it is left with '' then map command cannot match the integer value in usa_df_dropcol to string val in temp_category_mx
-category_usa= {int(old_key): val for old_key, val in temp_category_usa.items()}
-category_mx= {int(old_key): val for old_key, val in temp_category_mx.items()}
+#category_usa= {int(old_key): val for old_key, val in temp_category_usa.items()}
+#category_mx= {int(old_key): val for old_key, val in temp_category_mx.items()}
 
-#category_mx
+#category_usa
 
 
 # In[7]:
 
 
-print(category_usa == category_mx)
+category_mx={1: 'Film & Animation',
+ 2: 'Autos & Vehicles',
+ 10: 'Music',
+ 15: 'Pets & Animals',
+ 17: 'Sports',
+ 18: 'Short Movies',
+ 19: 'Travel & Events',
+ 20: 'Gaming',
+ 21: 'Videoblogging',
+ 22: 'People & Blogs',
+ 23: 'Comedy',
+ 24: 'Entertainment',
+ 25: 'News & Politics',
+ 26: 'Howto & Style',
+ 27: 'Education',
+ 28: 'Science & Technology',
+ 30: 'Movies',
+ 31: 'Anime/Animation',
+ 32: 'Action/Adventure',
+ 33: 'Classics',
+ 34: 'Comedy',
+ 35: 'Documentary',
+ 36: 'Drama',
+ 37: 'Family',
+ 38: 'Foreign',
+ 39: 'Horror',
+ 40: 'Sci-Fi/Fantasy',
+ 41: 'Thriller',
+ 42: 'Shorts',
+ 43: 'Shows',
+ 44: 'Trailers'}
+category_usa={1: 'Film & Animation',
+ 2: 'Autos & Vehicles',
+ 10: 'Music',
+ 15: 'Pets & Animals',
+ 17: 'Sports',
+ 18: 'Short Movies',
+ 19: 'Travel & Events',
+ 20: 'Gaming',
+ 21: 'Videoblogging',
+ 22: 'People & Blogs',
+ 23: 'Comedy',
+ 24: 'Entertainment',
+ 25: 'News & Politics',
+ 26: 'Howto & Style',
+ 27: 'Education',
+ 28: 'Science & Technology',
+ 29: 'Nonprofits & Activism',
+ 30: 'Movies',
+ 31: 'Anime/Animation',
+ 32: 'Action/Adventure',
+ 33: 'Classics',
+ 34: 'Comedy',
+ 35: 'Documentary',
+ 36: 'Drama',
+ 37: 'Family',
+ 38: 'Foreign',
+ 39: 'Horror',
+ 40: 'Sci-Fi/Fantasy',
+ 41: 'Thriller',
+ 42: 'Shorts',
+ 43: 'Shows',
+ 44: 'Trailers'}
 
 
 # In[8]:
 
 
-usa_df_dropcol=usa_df.drop(['video_id','thumbnail_link','description','tags','channel_title'], axis=1)
+print(category_usa == category_mx)
 
 
 # In[9]:
 
 
-mx_df_dropcol=mx_df.drop(['video_id','thumbnail_link','description','tags','channel_title'], axis=1)
+usa_df_dropcol=usa_df.drop(['video_id','thumbnail_link','description','tags','channel_title'], axis=1)
 
 
 # In[10]:
 
 
-usa_df_dropcol.head(10)
+mx_df_dropcol=mx_df.drop(['video_id','thumbnail_link','description','tags','channel_title'], axis=1)
 
 
 # In[11]:
 
 
-usa_df_dropcol['category_name']=usa_df_dropcol['category_id'].map(category_usa)
+usa_df_dropcol.head(10)
 
 
 # In[12]:
 
 
-mx_df_dropcol['category_name']=mx_df_dropcol['category_id'].map(category_mx)
+usa_df_dropcol['category_name']=usa_df_dropcol['category_id'].map(category_usa)
 
 
 # In[13]:
+
+
+mx_df_dropcol['category_name']=mx_df_dropcol['category_id'].map(category_mx)
+
+
+# In[14]:
 
 
 ####For use later
 ###usa_df_dropcol[(usa_df_dropcol['likes']== 0) & (usa_df_dropcol['ratings_disabled'] == False)]
 
 
-# In[14]:
+# In[15]:
 
 
 usa_df_dropcol.info()
 #From the info one can see that there are no null values in any of the cols for usa_df_dropcol
 
 
-# In[15]:
+# In[16]:
 
 
 mx_df_dropcol.info()
@@ -134,14 +208,14 @@ mx_df_dropcol.info()
 #Investingating
 
 
-# In[16]:
+# In[17]:
 
 
 temp_rows_drop=mx_df_dropcol
 temp_rows_drop.info()
 
 
-# In[17]:
+# In[18]:
 
 
 rowstodel=mx_df_dropcol[mx_df_dropcol['category_name'].isnull()].index
@@ -149,39 +223,39 @@ mx_df_dropcol.drop(rowstodel, inplace=True)
 mx_df_dropcol.info()
 
 
-# In[18]:
+# In[19]:
 
 
 usa_df_clean=usa_df_dropcol
 mx_df_clean=mx_df_dropcol
 
 
-# In[19]:
+# In[20]:
 
 
 pd.options.display.float_format = '{:,.1f}'.format
 usa_df_clean[['views','likes','dislikes','comment_count']].describe()
 
 
-# In[20]:
+# In[21]:
 
 
 mx_df_clean[['views','likes','dislikes','comment_count']].describe()
 
 
-# In[21]:
+# In[22]:
 
 
 #Searching Outliers
 
 
-# In[22]:
+# In[23]:
 
 
 #sns.boxplot(x=usa_df_clean['views'],showfliers = False)
 
 
-# In[23]:
+# In[24]:
 
 
 plt.subplot(1,2,1)
@@ -193,7 +267,7 @@ plt.title('Mexico-Outliers')
 sns.boxplot(x=mx_df_clean['views'])
 
 
-# In[24]:
+# In[25]:
 
 
 #import numpy as np
@@ -206,49 +280,49 @@ sns.boxplot(x=mx_df_clean['views'])
 #sns.boxplot(x=y[removed_outliers])
 
 
-# In[25]:
+# In[26]:
 
 
 temp_mx=mx_df_clean #saving the cleaned seperate US df in another temp_mx
 
 
-# In[26]:
+# In[27]:
 
 
 temp_usa=usa_df_clean #saving the cleaned seperate MX df in another temp_mx
 
 
-# In[27]:
+# In[28]:
 
 
 #Combining the two country's data together. Added another column 'country' to both cleaned dataframes and them appended MX under USA df. 
 
 
-# In[28]:
+# In[29]:
 
 
 mx_df_clean['country']='Mexico'
 
 
-# In[29]:
+# In[30]:
 
 
 usa_df_clean['country']='USA'
 
 
-# In[30]:
+# In[31]:
 
 
 combined_usa_mx_df=usa_df_clean.append(mx_df_clean)
 
 
-# In[31]:
+# In[32]:
 
 
 combined_usa_mx_df.info()
 
 
-# In[32]:
+# In[33]:
 
 
 #RESETTING INDEX AS APPENDING MX DF TO USA DF made the index out of order
@@ -264,7 +338,7 @@ combined_usa_mx_df.reset_index(inplace = True, drop = True)
 #remaining records=81051
 
 
-# In[33]:
+# In[34]:
 
 
 # Making trend_date and publish time as datetime objects
@@ -294,7 +368,7 @@ combined_usa_mx_df['trending_date']=pd.to_datetime(combined_usa_mx_df['trending_
 ###
 
 
-# In[34]:
+# In[35]:
 
 
 combined_usa_mx_df.info()
